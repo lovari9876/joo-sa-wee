727babd1bd69242dc2eba73276ba6a23050a3aac
lovari <lovari9876@gmail.com>
2020-04-02 19:11:32 +0900

    보부상: 검색 조건 말머리, searchCriteria 둘 다 해서 selectbox 두 개 다 넘기기 완성!!

--- sa-wee/src/main/java/com/soninlawisdice/service/SecondhandService.java
+++ sa-wee/src/main/java/com/soninlawisdice/service/SecondhandService.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.soninlawisdice.vo.SearchCriteria;
 import com.soninlawisdice.vo.TradeVO;
 
 public interface SecondhandService {
-	public ArrayList<HashMap<String, Object>> selectTradeList(SearchCriteria scri);
+	public ArrayList<HashMap<String, Object>> selectTradeList(SearchCriteria scri, String s_content);
 	
-	public int tradeListCount(SearchCriteria scri);
+	public int tradeListCount(SearchCriteria scri, String s_content);
 }
